4b6f942128b773392f6c87025ef56c9f670403f4
kentahayashi <kenta0810hayashi@gmail.com>
2023-08-09 23:58:39 +0200

    Programmatic navigation

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,11 +1,22 @@
 <script setup>
-defineProps({
+import { useRouter } from 'vue-router'
+const props = defineProps({
   event: {
     required: true,
   },
 })
+
+const router = useRouter()
+
+const register = () => {
+  router.push({
+    name: 'event-detail',
+    params: { id: props.event.id },
+  })
+}
 </script>
 
 <template>
   <p>Register for events here.</p>
+  <button @click="register">Register Me</button>
 </template>
